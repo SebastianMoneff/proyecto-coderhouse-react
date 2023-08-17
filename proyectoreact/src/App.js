c3083a5b6b79078eabe1fc1b5a7597eaac85c93e
Sebastian Moneff <sebastianmoneff@gmail.com>
2023-08-17 07:52:24 -0300

    Error en greeting y NavBar corregidas

--- proyectoreact/src/App.js
+++ proyectoreact/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import cart from "./assets/cart.png"
 import { ItenListContainer } from './components/ItemListContainer';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItenListContainer greetins= "Hola a todos!"/>
+      <ItenListContainer greeting= "Hola a todos!"/>
     </>
   )
 }
